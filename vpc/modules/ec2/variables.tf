8ccbf6b1af4bc5d2dcca9b5d5a6ddf6ef8e8554a
Akeron44 <73289118+Akeron44@users.noreply.github.com>
2024-11-07 16:56:25 +0100

    Add files via upload

---
+++ vpc/modules/ec2/variables.tf
@@ -0,0 +1,13 @@
+variable "public_subnet_id" {
+  description = "The id of the public subnet"
+  type        = string
+}
+variable "private_subnet_id" {
+  description = "The id of the public subnet"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The id of the vpc"
+  type        = string
+}
